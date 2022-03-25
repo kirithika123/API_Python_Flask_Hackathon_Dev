55e6245287a26062bb8324af07d8cf20f66096f1
kirithika MJ <krithika2010@yahoo.com>
2022-03-24 19:17:33 -0700

    changed procfile

--- configuration.py
+++ configuration.py
@@ -12,7 +12,7 @@ DATABASE_URL_DEV = 'postgresql://username:password@localhost/LMS_DB'
 where 'LMS_DB' is name of local postgreSQL DB  and <username> and <password> are the DB credentials
 """
 
-DATABASE_URL_DEV = ''
+DATABASE_URL_DEV = 'postgres://zobnofgthtlngf:e9ad1f6a91579c6a309aa680ee5fe433bdb8a3ac8b8c12e6aaa7c2a4a80d7e31@ec2-52-201-124-168.compute-1.amazonaws.com:5432/d20mmumha6j046'
 
 """
 USER_NAME - Needed for HTTP Basic auth. Insert the API username inside the empty string
@@ -20,7 +20,7 @@ E.g -
 USER_NAME = 'admin'
 """
 
-USER_NAME = ''
+USER_NAME = 'APIPROCESSING'
 
 """
 PASSWORD - Needed for HTTP Basic auth. Insert the API password inside the empty string
@@ -28,7 +28,7 @@ E.g -
 PASSWORD = 'password'
 """
 
-PASSWORD = ''
+PASSWORD = '2xx@Success'
 
 
 
